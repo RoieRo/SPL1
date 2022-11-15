f2a8a08d35373080b01c30173d8c4f47d243e5af
RoieRo <roieshton@gmail.com>
2022-11-15 20:45:59 +0200

    aviv

--- src/MandatesSelectionPolicy.cpp
+++ src/MandatesSelectionPolicy.cpp
@@ -1,1 +1,16 @@
 #include "../include/SelectionPolicy.h"
+void MandatesSelectionPolicy ::select(Simulation &s, vector<int> potential, int partyID, int agentId)
+{
+    int maxParty = -1;
+    int maxEdge = 0;
+    int edge;
+    for (int i = 0; i < potential.size(); i++)
+    {
+        edge = s.getParty(potential[i]).getMandates();
+        if (edge > maxEdge)
+        {
+            maxEdge = edge;
+            maxParty = i;
+        }
+    }
+}
